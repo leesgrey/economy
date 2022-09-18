c86c0f5fc2f2c81cb55c0ba87b57d2aa603c2de4
leesgrey <sieun.v.lee@gmail.com>
2022-09-18 01:43:48 -0400

    fixed where clauses, added reactions and item purchasing

--- bleh.py
+++ bleh.py
@@ -1,28 +1,32 @@
-from email import message
 import discord
 import json
 import peewee
 
 db = peewee.SqliteDatabase('economy.db')
 
-class User(peewee.Model):
+class BaseModel(peewee.Model):
+    class Meta:
+        database = db
+
+class User(BaseModel):
     discord_id = peewee.BigIntegerField()
     server_id = peewee.BigIntegerField()
     balance = peewee.BigIntegerField(default=0)
 
-    class Meta:
-        primary_key = peewee.CompositeKey('discord_id', 'server_id')
-        database = db
-
-class Item(peewee.Model):
+class Item(BaseModel):
     name = peewee.TextField()
     price = peewee.BigIntegerField()
     description = peewee.TextField(null=True)
     message = peewee.TextField(null=True)
     server_id = peewee.BigIntegerField()
 
-    class Meta:
-        database = db
+class Ownership(BaseModel):
+    owner_id = peewee.ForeignKeyField(User, User.discord_id)
+    server_id = peewee.ForeignKeyField(User, User.server_id) # can prob be removed
+    item_id = peewee.ForeignKeyField(Item)
+
+db.connect()
+db.create_tables([User, Item, Ownership])
 
 with open("config.json") as file:
     config = json.load(file)
@@ -32,49 +36,79 @@ intents.message_content = True
 activity = discord.Activity(type=discord.ActivityType.playing, name="god")
 client = discord.Client(intents=intents, activity=activity)
 
-db.connect()
-db.create_tables([User, Item])
-
 @client.event
 async def on_ready():
     print(f"hey im {client.user}")
 
 @client.event
-async def on_message(message):
-    if message.author == client.user:
+async def on_reaction_add(reaction, user):
+    if user.id == reaction.message.author.id or user.bot or reaction.message.author.bot:
         return
+
+    print(f"{user.display_name} reacted to {reaction.message.author.display_name} with {reaction.emoji}")
+
+    reacter = User.select().where((User.discord_id == user.id) & (User.server_id == reaction.message.guild.id)).first()
+    reacter.balance += 1
+    reacter.save()
+
+    reactee = User.select().where((User.discord_id == reaction.message.author.id) & (User.server_id == reaction.message.guild.id)).first()
+    reactee.balance += 1
+    reactee.save()
+    return
+
+@client.event
+async def on_message(message):
+    server_id = message.guild.id
+    user_id = message.author.id
+
+    user = User.select().where((User.discord_id == user_id) & (User.server_id == server_id)).first()
+    if user is None:
+        print(f"creating new user {message.author.display_name}")
+        user = User(discord_id=user_id, server_id=server_id)
+    user.balance += 1
+    print(f"incrementing {message.author.display_name}'s balance to {user.balance}")
+    user.save()
     
     if not message.content.startswith('$'):
         return
-
-    # check user in db
-    user = User.select().where(User.discord_id == message.author.id and User.server_id == message.guild.id).first()
-    if user is None:
-        user = User(discord_id=message.author.id, server_id=message.guild.id)
-        user.save()
     
     if message.content == "$":
-        await message.channel.send(f"u have {user.balance} dollars lol")
-
-    elif message.content == "$items":
-        for item in Item.select().where(Item.server_id == message.guild.id):
-            await message.channel.send(f"{item.name}: {item.price}")
+        await message.channel.send(f"{message.author.display_name} has {user.balance} dollars")
         return
 
-    elif message.content.startswith("$work"):
-        user.balance += 1
-        user.save()
+    elif message.content == "$shop":
+        items_msg = ""
+        for item in Item.select().where(Item.server_id == server_id):
+            items_msg += f"{item.name}: {item.price}\n"
+        await message.channel.send(items_msg)
+        return
+
+    elif message.content == "$inventory":
+        for ownership in Ownership.select().where((Ownership.server_id == server_id) & (Ownership.owner_id == message.author.id)):
+            item = Item.select().where(Item.id == ownership.item_id).first()
+            await message.channel.send(f"{item.name}")
+        return
 
     elif message.content.startswith("$create_item"):
         name, price, description, use_message = (message.content.split() + [None] * 4)[1:5]
-        if name == None or price == None < 2:
+        if name == None or price == None:
             await message.channel.send("$create_item *name* *price*")
             return
 
-        item = Item(name=name, server_id=message.guild.id, price=price, description=description, use_message=use_message)
+        item = Item(name=name, server_id=server_id, price=price, description=description, use_message=use_message)
         item.save()
         await message.channel.send(f"created item {name} with price {price}")
         return
     
+    elif message.content.startswith("$buy_item"):
+        item = (message.content.split() + [None])[1]
+        if item == None:
+            await message.channel.send("$create_item *item*")
+            return
+        
+        item = Item.select().where((Item.server_id == server_id) and (Item.name == item)).first()
+        ownership = Ownership(owner_id=message.author.id, server_id=server_id, item_id=item.id)
+        ownership.save()
+        await message.channel.send(f"bought {item.name} for {item.price} dollar")
 
 client.run(config["TOKEN"])
